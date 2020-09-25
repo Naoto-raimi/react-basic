abeb1bd522deb2f29cd0dafb5a5b92f33296021d
naoto tanaka <7010raimi@gmail.com>
2020-09-25 23:17:52 +0900

    [mod] eventハンドラ

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const Basic1 = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <div>
+      <button onClick={clickHandler}>Click</button>
       <h1>Hello React1 {props.name}</h1>
       <h1>Hello React2</h1>
     </div>
