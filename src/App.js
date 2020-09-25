7de768d305f06f2436f54a189388f61b11029096
naoto tanaka <7010raimi@gmail.com>
2020-09-26 00:15:46 +0900

    [add] useState 配列

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Basic1 from "./components/Basic1";
+import Basic2 from "./components/Basic2";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 />
+        <Basic2 />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
