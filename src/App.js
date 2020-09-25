a5c34806e65c8b84ec697c725ea1669a3528fbf9
naoto tanaka <7010raimi@gmail.com>
2020-09-25 23:11:25 +0900

    [mod] props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 />
+        <Basic1 name="Hooks" />
+        <Basic1 name="Native" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
