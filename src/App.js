4ae7a7d8457b9a3218314676c3378b2518594521
naoto tanaka <7010raimi@gmail.com>
2020-09-26 12:11:25 +0900

    [add] useEffect timer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import Basic2 from "./components/Basic2";
-import BasicUseEffect from "./components/BasicUseEffect";
+
+import TimerContainer from "./components/TimerContainer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2 /> */}
-        <BasicUseEffect />
+        <TimerContainer />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
