28b956a0108e3814125256526707cb10a4fea55e
naoto tanaka <7010raimi@gmail.com>
2020-09-27 21:35:59 +0900

    [mod] axios api fetch using id

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -3,23 +3,41 @@ import axios from "axios";
 
 const ApiFetch = () => {
   const [posts, setPosts] = useState([]);
+  const [id, setId] = useState(1);
+  const [clicked, setClicked] = useState(false);
+
+  const clickhandler = () => {
+    setClicked(!clicked);
+  };
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      setPosts(res.data);
-    });
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        setPosts(res.data);
+      });
     // fetch("https://jsonplaceholder.typicode.com/posts", { method: "GET" })
     //   .then((res) => res.json())
     //   .then((data) => setPosts(data));
-  }, []);
+  }, [clicked]);
 
   return (
     <div>
-      <ul>
+      <input
+        type="text"
+        value={id}
+        onChange={(evt) => setId(evt.target.value)}
+      />
+      <button type="button" onClick={clickhandler}>
+        Get Post
+      </button>
+      <br />
+      {posts.title}
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
